f7ac7229a9c64f5b40d97d8be860dbdfa236b1da
bhardwajshubh <43513682+bhardwajshubh@users.noreply.github.com>
2020-06-23 22:50:32 +0530

    bubble sort added

---
+++ sorting/bubbleSort.js
@@ -0,0 +1,20 @@
+// optimized bubble sort
+const bubbleSort = function(arr){
+	if(arr === null || arr.length === 0){
+		console.log("Please enter array to be sorted");
+		return;
+	}
+	let sorted = true;
+	for(let i = 0 ; i < arr.length-1 ; i++){
+		for(let j = 0 ; j < arr.length ; j++){
+			if(arr[j] > arr[j+1]){
+				sorted = false;
+				arr[j] = arr[j] + arr[j+1];
+				arr[j+1] = arr[j] - arr[j+1];
+				arr[j] = arr[j] - arr[j+1]; 	
+			}
+		}
+		if (sorted) break;
+	}
+	return arr;
+}
